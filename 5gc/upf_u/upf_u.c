db8f13a007421352aabaa36a6f6c96e566ef49d8
Vivek Jain <jain.vivek.anand@gmail.com>
2020-11-24 09:29:36 -0600

    upf_u: use advanced ring to support single threaded buffering functionality

--- 5gc/upf_u/upf_u.c
+++ 5gc/upf_u/upf_u.c
@@ -59,6 +59,25 @@
 #define NF_TAG "upf_u"
 
 #define SELF_IP 0
+#define PKT_READ_SIZE ((uint16_t)32)
+
+/* For advanced rings scaling */
+rte_atomic16_t signal_exit_flag;
+struct child_spawn_info {
+  struct onvm_nf_init_cfg *child_cfg;
+  struct onvm_nf *parent;
+};
+
+uint32_t buffer_length = 0;
+
+void sig_handler(int sig);
+
+void sig_handler(int sig) {
+  if (sig != SIGINT && sig != SIGTERM) return;
+
+  /* Will stop the processing for all spawned threads in advanced rings mode */
+  rte_atomic16_set(&signal_exit_flag, 1);
+}
 
 upf_pdr_t *GetPdrByUeIpAddress(struct rte_mbuf *pkt, uint32_t addr) {
   return NULL;
@@ -80,9 +99,13 @@ upf_pdr_t *GetPdrByTeid(struct rte_mbuf *pkt, uint32_t td) {
 
 upf_far_t *GetFarById(uint16_t id) { return NULL; }
 
+
+uint32_t packet_count = 0;
+
 static int packet_handler(
     __attribute__((unused)) struct rte_mbuf *pkt, struct onvm_pkt_meta *meta,
     __attribute__((unused)) struct onvm_nf_local_ctx *nf_local_ctx) {
+  printf("===============Packet count %u\n", ++packet_count);
   meta->action = ONVM_NF_ACTION_DROP;
 
   struct rte_ipv4_hdr *iph = onvm_pkt_ipv4_hdr(pkt);
@@ -172,6 +195,13 @@ static int packet_handler(
         // TODO(vivek): Implement forward policy
         break;
       case FAR_BUFFER:
+        if (far->curr_cap < MAX_BUFFER_LENGTH) {
+          far->curr_cap++;
+          printf("Buffering the packet, total packet in the buffer: %d\n", far->curr_cap);
+        } else {
+          printf("Dropping packet due to buffer full: %d\n", far->curr_cap);
+        }
+        break;
       // TODO(vivek): Implement buffering policy
       case FAR_NOTIFY_CP:
       // TODO(vivek): Implement notify CP policy
@@ -186,16 +216,98 @@ static int packet_handler(
   return 0;
 }
 
+static int
+thread_main_loop(struct onvm_nf_local_ctx *nf_local_ctx) {
+        void *pkts[PKT_READ_SIZE];
+        struct onvm_pkt_meta *meta;
+        uint16_t i, nb_pkts;
+        struct rte_mbuf *pktsTX[PKT_READ_SIZE];
+        int tx_batch_size;
+        struct rte_ring *rx_ring;
+        struct rte_ring *msg_q;
+        struct onvm_nf *nf;
+        struct onvm_nf_msg *msg;
+        struct rte_mempool *nf_msg_pool;
+
+        nf = nf_local_ctx->nf;
+
+        onvm_nflib_nf_ready(nf);
+
+        /* Get rings from nflib */
+        rx_ring = nf->rx_q;
+        msg_q = nf->msg_q;
+        nf_msg_pool = rte_mempool_lookup(_NF_MSG_POOL_NAME);
+
+        printf("Process %d handling packets using advanced rings\n", nf->instance_id);
+        if (onvm_threading_core_affinitize(nf->thread_info.core) < 0)
+                rte_exit(EXIT_FAILURE, "Failed to affinitize to core %d\n", nf->thread_info.core);
+
+        uint16_t chalne_do = 1;
+        int error_code = 0;
+        while (!rte_atomic16_read(&signal_exit_flag)) {
+                /* Check for a stop message from the manager */
+                if (unlikely(rte_ring_count(msg_q) > 0)) {
+                        msg = NULL;
+                        rte_ring_dequeue(msg_q, (void **)(&msg));
+                        if (msg->msg_type == MSG_STOP) {
+                                rte_atomic16_set(&signal_exit_flag, 1);
+                        } else if (msg->msg_type == MSG_FROM_NF) {
+                          printf("Received message from NF\n");
+                          struct FlushBufferMessage * message;
+                          message = (struct FlushBufferMessage *) msg->msg_data;
+                          if (message != NULL) {
+                            if (message->far != NULL && message->far->apply_action == FAR_BUFFER) {
+                              printf("%s the %u packets from buffer based on the new action\n", message->new_action == FAR_DROP? "FAR_DROP" : "FAR_FORWARD", message->far->curr_cap);
+                              message->far->curr_cap = 0;
+                              message->far->apply_action = message->new_action;
+                            } else {
+                              printf("Current action is not buffering any packet, something went wrong\n");
+                            }
+                          }
+                        } else {
+                                printf("Received message %d, ignoring", msg->msg_type);
+                        }
+                        rte_mempool_put(nf_msg_pool, (void *)msg);
+                }
+
+                tx_batch_size = 0;
+                nb_pkts = rte_ring_dequeue_burst(rx_ring, pkts, PKT_READ_SIZE, NULL);
+
+                /* Process all the dequeued packets */
+                for (i = 0; i < nb_pkts; i++) {
+                        meta = onvm_get_pkt_meta((struct rte_mbuf *)pkts[i]);
+                        packet_handler((struct rte_mbuf *)pkts[i], meta, nf_local_ctx);
+                        pktsTX[tx_batch_size++] = pkts[i];
+                }
+
+                onvm_pkt_process_tx_batch(nf->nf_tx_mgr, pktsTX, tx_batch_size, nf);
+                if (tx_batch_size < PACKET_READ_SIZE) {
+                        onvm_pkt_flush_all_nfs(nf->nf_tx_mgr, nf);
+                }
+        }
+        return 0;
+}
 int main(int argc, char *argv[]) {
   int arg_offset;
   struct onvm_nf_local_ctx *nf_local_ctx;
   struct onvm_nf_function_table *nf_function_table;
 
   nf_local_ctx = onvm_nflib_init_nf_local_ctx();
-  onvm_nflib_start_signal_handler(nf_local_ctx, NULL);
+#ifdef BUFFER_MODE
+  printf("Using advanced mode\n");
+  /* If we're using advanced rings also pass a custom cleanup function,
+   * this can be used to handle NF specific (non onvm) cleanup logic */
+  rte_atomic16_init(&signal_exit_flag);
+  rte_atomic16_set(&signal_exit_flag, 0);
+  onvm_nflib_start_signal_handler(nf_local_ctx, sig_handler);
 
+  /* No need to define a function table as adv rings won't run onvm_nflib_run */
+  nf_function_table = NULL;
+#else
+  onvm_nflib_start_signal_handler(nf_local_ctx, NULL);
   nf_function_table = onvm_nflib_init_nf_function_table();
   nf_function_table->pkt_handler = &packet_handler;
+#endif
 
   if ((arg_offset = onvm_nflib_init(argc, argv, NF_TAG, nf_local_ctx,
                                     nf_function_table)) < 0) {
@@ -213,7 +325,12 @@ int main(int argc, char *argv[]) {
 
   PfcpSessionTableNFInit();
 
+#ifdef BUFFER_MODE
+  thread_main_loop(nf_local_ctx);
+#else
   onvm_nflib_run(nf_local_ctx);
+#endif
+
 
   onvm_nflib_stop(nf_local_ctx);
   printf("If we reach here, program is ending\n");
